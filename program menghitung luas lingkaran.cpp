48c22160e7334765ecd2b39d0c4683af543fec06
risva palintya <45660844+Risvapalintya@users.noreply.github.com>
2019-01-04 14:57:32 +0700

    Add files via upload

---
+++ program menghitung luas lingkaran.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r;
+    float phi=3.14,luas;
+    cout<<"MENGHITUNG LUAS LINGKARAN"<<endl;
+    cout<<"masukan jari-jari lingkaran:";
+    cin>>r;
+    luas=phi*r*r;
+    cout<<"luas lingkaran adalah:"<<luas<<endl;
+    return 0;
+}
